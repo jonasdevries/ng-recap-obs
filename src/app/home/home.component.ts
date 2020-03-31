5ddd4c9ee8564f8ae654a15233e544b92829f305
Jonas Devries <jonas@good-it.be>
2020-03-31 20:01:09 +0200

    first observable with subscription and unsubscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import {interval} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    interval(1000)
+    this.firstSubscription = interval(1000)
       .subscribe((count) => {
         console.log(count);
       });
+  }
+
+  ngOnDestroy(): void {
+    this.firstSubscription.unsubscribe();
   }
 
 }
